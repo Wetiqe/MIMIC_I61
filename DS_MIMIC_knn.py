0c4d0121e32a00a7342b53f7b6601f7c11f7dff9
AEMC2 <47979312+AEMC2@users.noreply.github.com>
2022-05-09 09:48:38 +0100

    Add files via upload
    Delete print and training

--- DS_MIMIC_knn.py
+++ DS_MIMIC_knn.py
@@ -6,14 +6,22 @@ Created on Mon May  2 15:14:58 2022
 """
 
 # -*- coding: utf-8 -*-
+from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import operator
 import matplotlib.pyplot as plt
 
-def My_knncomper_difftest(k_max, X_train_std, X_test_std, Y_train, Y_test):
-    Train_acc=[]; Test_acc=[]; acc=[]
-    for k in range(1, k_max):
+def My_knncomper_difftest(kmax):
+    rightnumber = 0           
+    print('enter phase 2')
+    Train_acc=[]
+    Test_acc=[]
+    acc=[]
+    
+    print(" maximum K value is "+str(kmax))           
+    for k in range(1,kmax):
         test_predict_score=0
         rightnumber=0
         predictlist =MYknn(X_test_std, X_train_std, Y_train, k)
@@ -21,9 +29,8 @@ def My_knncomper_difftest(k_max, X_train_std, X_test_std, Y_train, Y_test):
             if predictlist[m] == Y_test[m]:
                 rightnumber = rightnumber + 1
         test_predict_score = (rightnumber / len(Y_test))
-        print(k, "\t\t", test_predict_score*100)
+        print(test_predict_score*100)
         Test_acc.append(  test_predict_score * 100)
-
         rightnumber=0
         Train_predict_score=0
         predictlist = MYknn(X_train_std, X_train_std, Y_train, k)
@@ -31,15 +38,16 @@ def My_knncomper_difftest(k_max, X_train_std, X_test_std, Y_train, Y_test):
             if predictlist[m] == Y_train[m]:
                 rightnumber = rightnumber + 1
         Train_predict_score = (rightnumber / len(X_train_std))
-        Train_acc.append( Train_predict_score * 100)
+        Train_acc.append( Train_predict_score * 100)      
+     
     plt.plot(Test_acc)
     plt.title('Test_acc')
     plt.show()
     plt.plot(Train_acc)
     plt.title('Train_acc')
     plt.show()
-    
 
+        
 def MYknn(test_object, training_object, training_object_target, K):
     predictlist = []
     for newpoint in test_object:
@@ -53,8 +61,19 @@ def MYknn(test_object, training_object, training_object_target, K):
             voteIlabel = training_object_target[sortedDistIndicies[i]]
             classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1  #Get the value of key from the map and return 0 without key
             sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
-            print(sortedClassCount)
-            print(type(sortedClassCount))
         predictlist.append(sortedClassCount[0][0])
-        
-        return predictlist
+    return predictlist
+
+
+
+if __name__ == '__main__':
+    MIMIC_Data=pd.read_csv('E:\\MIMIC_DS\\test2.csv') 
+    MIMIC_Y = MIMIC_Data.pop('target').values
+    MIMIC_X = MIMIC_Data.values
+    X_train, X_test, Y_train, Y_test = train_test_split(MIMIC_X, MIMIC_Y, test_size=0.2,random_state=40)  # the value will sa
+    
+    std= StandardScaler()
+    X_train_std=std.fit_transform(X_train)
+    X_test_std=std.fit_transform(X_test)
+    print('use my knn')
+    My_knncomper_difftest(15)
